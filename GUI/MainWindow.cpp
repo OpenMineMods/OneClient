3969e176e12d29dd275309832e7f220d18edbd62
Joona <joonatoona@digitalfishfun.com>
2017-10-15 12:45:50 -0700

    folder paths

--- GUI/MainWindow.cpp
+++ GUI/MainWindow.cpp
@@ -1,11 +1,15 @@
 #include "MainWindow.h"
 #include "utils.h"
+#include <iostream>
+#include <QStandardPaths>
 
 MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags flags) : QMainWindow(parent, flags) {
     _ui.setupUi(this);
 
-    _ui.analytics_check->setChecked(true);
-
     QString style = Utils::loadStyleSheet("main");
     this->setStyleSheet(style);
+
+    _config_dir = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
+    _data_dir = QStandardPaths::writableLocation(QStandardPaths::DataLocation);
+    _cache_dir = QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
 }
--- GUI/MainWindow.h
+++ GUI/MainWindow.h
@@ -9,4 +9,8 @@ public:
     MainWindow(QWidget* parent = 0, Qt::WindowFlags flags = 0);
 
     Ui_MainWindow _ui;
+
+    QString _config_dir;
+    QString _data_dir;
+    QString _cache_dir;
 };
