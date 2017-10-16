51e59b499f8502b1988ef1f31a0e6d56e09a6c24
Joona <joonatoona@digitalfishfun.com>
2017-10-15 21:07:20 -0700

    authors and mc version

--- GUI/PackWidget.cpp
+++ GUI/PackWidget.cpp
@@ -6,4 +6,12 @@ PackWidget::PackWidget(const CurseMetaDB::CurseProject* project, QWidget* parent
     _ui.pack_name->setText(project->title);
     _ui.pack_desc->setText(project->desc);
     _ui.pack_downloads->setText("Downloads: " + QString::number(project->downloads));
+
+    QStringList authors;
+    for (int i = 0; i < project->authors.size(); ++i) {
+        authors.append(project->authors[i].toString());
+    }
+
+    _ui.pack_authors->setText("By " + authors.join(", "));
+    _ui.pack_mcver->setText("Minecraft " + project->versions[project->versions.size() - 1].toString());
 }
